bb64b2f5f7664ac6718a241166d83c5ef53f2fe1
Erica Morse <ericamorse@ericamorse-macbookpro2.roam.corp.google.com>
2014-06-15 20:33:15 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Erica Morse on 6/15/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
