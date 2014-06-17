2887bdd9904b0ad3c132d20f4466eca800868962
morsematters <ericamorse@ericamorse-macbookpro2.roam.corp.google.com>
2014-06-16 22:32:29 -0700

    project updates

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Erica Morse on 6/15/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
